790a2cdec0925e116f5a445d36bc0dc267eb6e69
Mallory Standridge <mallory.standridge@gmail.com>
2013-03-19 00:17:44 -0400

    program looping
    section 3… printing tables

---
+++ chapter_5/section_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (void)
+{
+	int triangularNumber;  
+	
+	triangularNumber = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8;
+	
+	printf ("The eighth triangular number is %i\n", triangularNumber);
+		
+	return 0;
+}
+
+
+OUTPUT
+The eighth triangular number is 36 
