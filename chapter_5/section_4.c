f11e81919362480e77533aaa5a0e4a3bfc2a2430
Mallory Standridge <mallory.standridge@gmail.com>
2013-03-19 00:39:14 -0400

    asking the user for input

---
+++ chapter_5/section_4.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+int main (void)
+{
+	int n, number, triangularNumber;  
+	
+	printf ("What triangular number do you want?");
+	scanf ("%i", &number);
+	
+	triangularNumber = 0;
+	
+	for ( n = 1; n <= number; ++n)
+		triangularNumber += n;
+	
+	printf ("Triangular number %i is %i\n", number, triangularNumber);
+
+	return 0;
+}
+
+OUTPUT
+What triangular number do you want? 100
+Triangular number 100 is 5050
+
